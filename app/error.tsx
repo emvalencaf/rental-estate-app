681b1611e8d0a2373b042361fd2e05b3b7feccdc
emvalencaf <edsonmvf@gmail.com>
2023-06-21 19:09:37 -0300

    Update error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -2,14 +2,17 @@
 
 // hooks
 import { useEffect } from "react";
-import { EmptyState } from "./components";
+
+//import { EmptyState } from "./components";
 
 interface ErrorStateProps {
     error: Error;
+    reset: () => void;
 }
 
 const ErrorState: React.FC<ErrorStateProps> = ({
     error,
+    reset,
 }) => {
 
     useEffect(() => {
@@ -17,10 +20,41 @@ const ErrorState: React.FC<ErrorStateProps> = ({
     }, [error])
 
     return (
-        <EmptyState
-            title="Uh oh"
-            subtitle="Something went wrong"
-        />
+        <div
+            className="h-[60vh]
+                flex flex-col gap-2
+                justify-center
+                items-center">
+            <div className="text-center">
+                <div className="text-2xl font-bold">Uh Oh</div>
+                <div className="font-light text-neutral-500 mt-2">
+                    Something went wrong!
+                </div>
+            </div>
+            <div
+                className="w-48 mt-4
+        ">
+                <button
+                    onClick={
+                        // Attempt to recover by trying to re-render the segment
+                        () => reset()
+                    }
+                    className=" 
+                        relative
+                        disabled:opacity-70
+                        disabled:cursor-not-allowed
+                        rounded-lg
+                        hover:opacity-80
+                        transition
+                        w-full
+                       bg-white
+                       border-black
+                       text-black
+                       ">
+                    Reset
+                </button>
+            </div>
+        </div>
     );
 }
 
